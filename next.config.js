801e2ab46f1442549b1719d8e362a26390f3700e
lenwoper <abhijeetkumarlucknow@gmail.com>
2023-01-16 09:53:28 -0800

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    loader:"akamai",
+    path:""
+  },
+  basePath:"/portfolio",
+  assetPrefix:"/portfolio"
 }
 
 module.exports = nextConfig
